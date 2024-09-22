0d9b5616e2301c163d208a9979457cd8a94ce711
Kushal0jain <jainkushal0909@gmail.com>
2024-09-22 14:29:38 +0530

    First git

---
+++ src/BankingManagementSystem/BankingApp.java
@@ -0,0 +1,121 @@
+package BankingManagementSystem;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class BankingApp {
+    private static final String url = "jdbc:mysql://localhost:3306/banking_system";
+    private static final String username = "root";
+    private static final String password = "1256";
+
+    public static void application() throws ClassNotFoundException, SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            Scanner scanner = new Scanner(System.in);
+            User user = new User(connection, scanner);
+            Accounts accounts = new Accounts(connection, scanner);
+            AccountManager accountManager = new AccountManager(connection, scanner);
+
+            String email;
+            long account_number;
+
+            while (true) {
+                System.out.println("*** WELCOME TO BANKING SYSTEM ***");
+                System.out.println();
+                System.out.println("1. Register");
+                System.out.println("2. Login");
+                System.out.println("3. Exit");
+                System.out.print("Enter your choice: ");
+                int choice1 = scanner.nextInt();
+
+                switch (choice1) {
+                    case 1:
+                        user.register();
+                        break;
+
+                    case 2:
+                        email = user.login();
+                        if (email != null) {
+                            System.out.println();
+                            System.out.println("User Logged In!");
+
+                            // If no account exists, ask to open one
+                            if (!accounts.account_exist(email)) {
+                                System.out.println();
+                                System.out.println("1. Open a new Bank Account");
+                                System.out.println("2. Exit");
+                                if (scanner.nextInt() == 1) {
+                                    account_number = accounts.open_account(email);
+                                    System.out.println("Account Created Successfully");
+                                    System.out.println("Your Account Number is: " + account_number);
+                                } else {
+                                    break;
+                                }
+                            }
+
+                            // Fetch account number
+                            account_number = accounts.getAccount_number(email);
+                            int choice2 = 0;
+
+                            // Logged in menu loop
+                            while (true) {
+                                System.out.println();
+                                System.out.println("1. Debit Money");
+                                System.out.println("2. Credit Money");
+                                System.out.println("3. Transfer Money");
+                                System.out.println("4. Check Balance");
+                                System.out.println("5. Log Out");
+                                System.out.print("Enter your choice: ");
+                                choice2 = scanner.nextInt();
+
+                                switch (choice2) {
+                                    case 1:
+                                        accountManager.debit_money(account_number);
+                                        break;
+                                    case 2:
+                                        accountManager.credit_money(account_number);
+                                        break;
+                                    case 3:
+                                        accountManager.transfer_money(account_number);
+                                        break;
+                                    case 4:
+                                        accountManager.getBalance(account_number);
+                                        break;
+                                    case 5:
+                                        System.out.println("Logging out...");
+                                        break;
+                                    default:
+                                        System.out.println("Enter Valid Choice!");
+                                }
+
+                                // If user selects Log Out (choice 5), break the inner loop
+                                if (choice2 == 5) {
+                                    break;
+                                }
+                            }
+                        } else {
+                            System.out.println("Incorrect Email or Password!");
+                        }
+                        break;
+
+                    case 3:
+                        System.out.println("THANK YOU FOR USING BANKING SYSTEM!!!");
+                        System.out.println("Exiting System!");
+                        return;
+
+                    default:
+                        System.out.println("Enter Valid Choice");
+                        break;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/BankingManagementSystem/Main.java
@@ -0,0 +1,12 @@
+package BankingManagementSystem;
+
+import java.sql.SQLException;
+
+import static BankingManagementSystem.BankingApp.application;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        application();
+    }
+}
